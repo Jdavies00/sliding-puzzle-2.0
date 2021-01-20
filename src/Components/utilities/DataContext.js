176a40b0f08f4fb3550365b050d6b80ad4943e60
James Davies <j.davies2099@gmail.com>
2021-01-20 17:36:34 -0500

    semi functioning, need bootstrap style

---
+++ src/Components/utilities/DataContext.js
@@ -0,0 +1,11 @@
+import React, { useState, useContext, createContext } from "react";
+
+function DataContext() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DataContext
