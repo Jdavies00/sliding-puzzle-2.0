e08b4504b1cd99d25d95d4e96e7ef2b788a10802
James Davies <j.davies2099@gmail.com>
2021-01-25 20:27:37 -0500

    app.js is closer to final product

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ class App extends Component {
     super();
     this.state = {
       dataForTheTile: [
-        { 'id': 0, 'currentValue': 0, 'possibleMoves': [1, 4], 'currentRowX': 0, 'currentCloumY': 0, 'winPosition': [0, 0] },
-        { 'id': 1, 'currentValue': 1, 'possibleMoves': [0, 2, 5], 'currentRowX': 1, 'currentCloumY': 0, 'winPosition': [1, 0] },
-        { 'id': 2, 'currentValue': 2, 'possibleMoves': [1, 6, 3], 'currentRowX': 2, 'currentCloumY': 0, 'winPosition': [2, 0] },
-        { 'id': 3, 'currentValue': 3, 'possibleMoves': [2, 7], 'currentRowX': 3, 'currentCloumY': 0, 'winPosition': [3, 0] },
+        { 'id': 0, 'currentValue': 0, 'possibleMoves': [1, 4], 'currentRowX': 0, 'currentCloumY': 0, 'winValue': 0 },
+        { 'id': 1, 'currentValue': 1, 'possibleMoves': [0, 2, 5], 'currentRowX': 1, 'currentCloumY': 0, 'winValue': [1, 0] },
+        { 'id': 2, 'currentValue': 2, 'possibleMoves': [1, 6, 3], 'currentRowX': 2, 'currentCloumY': 0, 'winValue': [2, 0] },
+        { 'id': 3, 'currentValue': 3, 'possibleMoves': [2, 7], 'currentRowX': 3, 'currentCloumY': 0, 'winValue': [3, 0] },
 
-        { 'id': 4, 'currentValue': 4, 'possibleMoves': [0, 5, 8], 'currentRowX': 0, 'currentCloumY': 1, 'winPosition': [0, 1] },
-        { 'id': 5, 'currentValue': 5, 'possibleMoves': [1, 4, 6, 9], 'currentRowX': 1, 'currentCloumY': 1, 'winPosition': [1, 1] },
-        { 'id': 6, 'currentValue': 6, 'possibleMoves': [2, 5, 10, 7], 'currentRowX': 2, 'currentCloumY': 1, 'winPosition': [2, 1] },
-        { 'id': 7, 'currentValue': 7, 'possibleMoves': [3, 6, 11], 'currentRowX': 3, 'currentCloumY': 1, 'winPosition': [3, 1] },
+        { 'id': 4, 'currentValue': 4, 'possibleMoves': [0, 5, 8], 'currentRowX': 0, 'currentCloumY': 1, 'winValue': [0, 1] },
+        { 'id': 5, 'currentValue': 5, 'possibleMoves': [1, 4, 6, 9], 'currentRowX': 1, 'currentCloumY': 1, 'winValue': [1, 1] },
+        { 'id': 6, 'currentValue': 6, 'possibleMoves': [2, 5, 10, 7], 'currentRowX': 2, 'currentCloumY': 1, 'winValue': [2, 1] },
+        { 'id': 7, 'currentValue': 7, 'possibleMoves': [3, 6, 11], 'currentRowX': 3, 'currentCloumY': 1, 'winValue': [3, 1] },
 
-        { 'id': 8, 'currentValue': 8, 'possibleMoves': [4, 9, 12], 'currentRowX': 0, 'currentCloumY': 2, 'winPosition': [0, 2] },
-        { 'id': 9, 'currentValue': 9, 'possibleMoves': [5, 8, 10, 13], 'currentRowX': 1, 'currentCloumY': 2, 'winPosition': [1, 2] },
-        { 'id': 10, 'currentValue': 10, 'possibleMoves': [6, 9, 14, 11], 'currentRowX': 2, 'currentCloumY': 2, 'winPosition': [2, 2] },
-        { 'id': 11, 'currentValue': 11, 'possibleMoves': [7, 10, 15], 'currentRowX': 3, 'currentCloumY': 2, 'winPosition': [3, 2] },
+        { 'id': 8, 'currentValue': 8, 'possibleMoves': [4, 9, 12], 'currentRowX': 0, 'currentCloumY': 2, 'winValue': [0, 2] },
+        { 'id': 9, 'currentValue': 9, 'possibleMoves': [5, 8, 10, 13], 'currentRowX': 1, 'currentCloumY': 2, 'winValue': [1, 2] },
+        { 'id': 10, 'currentValue': 10, 'possibleMoves': [6, 9, 14, 11], 'currentRowX': 2, 'currentCloumY': 2, 'winValue': [2, 2] },
+        { 'id': 11, 'currentValue': 11, 'possibleMoves': [7, 10, 15], 'currentRowX': 3, 'currentCloumY': 2, 'winValue': [3, 2] },
 
-        { 'id': 12, 'currentValue': 12, 'possibleMoves': [8, 13], 'currentRowX': 0, 'currentCloumY': 3, 'winPosition': [0, 3] },
-        { 'id': 13, 'currentValue': 13, 'possibleMoves': [9, 12, 14], 'currentRowX': 1, 'currentCloumY': 3, 'winPosition': [1, 3] },
-        { 'id': 14, 'currentValue': 14, 'possibleMoves': [10, 13, 15], 'currentRowX': 2, 'currentCloumY': 3, 'winPosition': [2, 3] },
-        { 'id': 15, 'currentValue': 15, 'possibleMoves': [11, 14], 'currentRowX': 3, 'currentCloumY': 3, 'winPosition': [3, 3] }],
+        { 'id': 12, 'currentValue': 12, 'possibleMoves': [8, 13], 'currentRowX': 0, 'currentCloumY': 3, 'winValue': [0, 3] },
+        { 'id': 13, 'currentValue': 13, 'possibleMoves': [9, 12, 14], 'currentRowX': 1, 'currentCloumY': 3, 'winValue': [1, 3] },
+        { 'id': 14, 'currentValue': 14, 'possibleMoves': [10, 13, 15], 'currentRowX': 2, 'currentCloumY': 3, 'winValue': [2, 3] },
+        { 'id': 15, 'currentValue': 15, 'possibleMoves': [11, 14], 'currentRowX': 3, 'currentCloumY': 3, 'winValue': [3, 3] }],
       Clicks: 0
     }
     this.clickfunction = this.clickfunction.bind(this)
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,48 +3,48 @@ import { Container, Button } from "reactstrap"
 
 function Board(props) {
     const [tileButton, setTileButton] = useState([])
-    const [tileDataArray, setTileDataArray] = useState([]);
+    const [alltileData, setAllTileData] = useState([]);
     
     
 
 
-    let arrayTileData = []
+    let tileDataArray = []
     for (let i = 0; i < 16; i++) {
         let tileData = { "id": (i), "currentValue": (i), "possibleMoves": [(i + 1), (i - 1), (i + 4), (i - 4)], "winValuess":(i) }
         // useState.setTileDataArray(tileData)
-        arrayTileData.push(tileData)
-        console.log(tileDataArray)
-        console.log(arrayTileData)
+        tileDataArray.push(tileData)
+        console.log(tileDataArray.length)
+        // console.log(arrayTileData)
     }
 
 
+    const updateTile = ()=>
+        setAllTileData([...tileDataArray,{
+            id:tileDataArray.id,
+            value:tileDataArray.currentValue
+        }])
 
     let buttonArray = []
     for (let i = 0; i < props.tile_Data_Raw.length; i++) {
         let num = props.tile_Data_Raw[i]
         let ogButton = <Button outline color="primary" 
-        onClick={() => props.tile_click(i)} style={{ height: 150, width: 250 }} key={i}>{num.currentValue}
+        onClick={() => props.tile_click(i)} 
+        // onClick={updateTile}
+        style={{ height: 150, width: 250 }} key={i}>{num.currentValue}
         </Button>
         buttonArray.push(ogButton)
     }
 
-    const additem = ()=>
-        setTileDataArray([...tileDataArray,{
-            id:tileDataArray.length,
-            value:Math.floor(Math.random()*10)+1
-        }])
     return (
         <>
         <Container>
             {buttonArray}
         </Container>
-        <Button onClick={additem}></Button>
-        <ul>
-            {tileDataArray.map(tileDataSingle =><li key={tileDataSingle.id}>{tileDataSingle.currentValue}</li>)}
-        </ul>
-        <Container>
-            
-        </Container>
+        {/* <Button onClick={updateTile}></Button> */}
+        {/* <ul> */}
+            {/* {tileDataArray.map(tileDataSingle =><li key={tileDataSingle.id}>{tileDataSingle.currentValue}</li>)} */}
+        {/* </ul> */}
+       
         </>
     )
 }
